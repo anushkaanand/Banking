dc1678120dfbdc879a7809e69e0df63a3c5ce5ae
Sheth <dishant.nirov.sheth@intel.com>
2019-10-12 19:25:05 +0530

    mysql connection

--- src/main/java/POJO/Accounts.java
+++ src/main/java/POJO/Accounts.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Accounts
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int accountID;
 
     private int custID;
