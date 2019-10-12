7287aa7acaedeb0c4ee9491c735c484a1744d297
Sheth <dishant.nirov.sheth@intel.com>
2019-10-12 19:33:39 +0530

    mysql connection

--- src/main/java/POJO/Accounts.java
+++ src/main/java/POJO/Accounts.java
@@ -1,6 +1,7 @@
 package main.java.POJO;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
