f1f95033fbf93e51fa166caddca5212f8e3b2fc0
Sheth <dishant.nirov.sheth@intel.com>
2019-10-12 19:00:15 +0530

    mysql connection

--- src/main/java/AccountsAPI.java
+++ src/main/java/AccountsAPI.java
@@ -5,6 +5,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @EnableAutoConfiguration
 public class AccountsAPI{
@@ -13,6 +15,6 @@ public class AccountsAPI{
 
     @PostMapping("/accounts/")
     String createAcc(@RequestBody Map<String, String> account){
-        return (accSvc.create(account) == null) ? "Failed" : "Success";
+        return (accSvc.create(account)) ? "Success" : "Failed";
     }
 }
